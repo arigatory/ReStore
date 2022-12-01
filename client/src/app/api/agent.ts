a3b01e59abb2235700d3053d776f3ad0985704f7
Ivan MSI <arigatory@gmail.com>
2022-12-01 23:58:06 +0300

    handle validation errors

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,8 +1,42 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response! as any;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modelStateErrors: string[] = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modelStateErrors.flat();
+        }
+        toast.error(data.title);
+        break;
+      case 401:
+        toast.error(data.title);
+        break;
+      case 500:
+        toast.error(data.title);
+        break;
+      default:
+        break;
+    }
+    console.log('caught by interceptor');
+    return Promise.reject(error.response);
+  }
+);
 
 const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
@@ -13,8 +47,8 @@ const requests = {
 
 const Catalog = {
   list: () => requests.get('products'),
-  details: (id: number) => requests.get(`products/${id}`)
-}
+  details: (id: number) => requests.get(`products/${id}`),
+};
 
 const TestErrors = {
   get400Error: () => requests.get('buggy/bad-request'),
@@ -22,11 +56,11 @@ const TestErrors = {
   get404Error: () => requests.get('buggy/not-found'),
   get500Error: () => requests.get('buggy/server-error'),
   getValidationError: () => requests.get('buggy/validation-error'),
-}
+};
 
 const agent = {
   Catalog,
-  TestErrors
-}
+  TestErrors,
+};
 
 export default agent;
--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,9 +1,80 @@
-import { Typography } from '@mui/material'
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import agent from '../../app/api/agent';
 
 export default function AboutPage() {
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  function getValidationError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log('Should not see this'))
+      .catch((error) => setValidationErrors(error));
+  }
+
   return (
-    <Typography>
-      About Page
-    </Typography>
-  )
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((e) => console.log(e))
+          }
+        >
+          Test 400 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((e) => console.log(e))
+          }
+        >
+          Test 401 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((e) => console.log(e))
+          }
+        >
+          Test 404 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((e) => console.log(e))
+          }
+        >
+          Test 500 Error
+        </Button>
+        <Button variant="contained" onClick={getValidationError}>
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+      {validationErrors!.length > 0 && (
+        <Alert severity="error">
+          <AlertTitle>Validation Error</AlertTitle>
+          <List>
+            {validationErrors.map((e) => (
+              <ListItem key={e}>
+                <ListItemText>{e}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  );
 }
