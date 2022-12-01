8278be2f670804f2188a4f36ba7eeafdba4af939
Ivan MSI <arigatory@gmail.com>
2022-12-01 03:04:40 +0300

    agent

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -8,7 +8,6 @@ import {
   TableRow,
   Typography,
 } from '@mui/material';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import agent from '../../app/api/agent';
